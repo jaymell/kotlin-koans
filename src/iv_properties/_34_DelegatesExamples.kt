a7b9333df4fce93e0a1c0549073ae15f0d2921db
James Lucas <jamesmlucas@gmail.com>
2017-07-16 02:46:26 -0500

    34

--- src/iv_properties/_34_DelegatesExamples.kt
+++ src/iv_properties/_34_DelegatesExamples.kt
@@ -2,9 +2,11 @@ package iv_properties
 
 import util.TODO
 import util.doc34
+import kotlin.reflect.*
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+    // derp: should have used lazy() here -- would have been one line
+    val lazyValue: Int by LazyDelegate(initializer)
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
@@ -14,3 +16,22 @@ fun todoTask34(): Lazy<Int> = TODO(
     """,
     documentation = doc34()
 )
+
+// this should not exist
+class LazyDelegate(val initializer: () -> Int) {
+    var initialized: Boolean = false
+    var myVar: Int = 0
+
+	operator fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+           if (initialized == false) {
+              initialized = true
+              myVar = initializer()
+          }
+          return myVar
+	}
+
+	operator fun setValue(thisRef: Any?,
+		property: KProperty<*>, value: Int) {
+		myVar = value
+	}
+}
