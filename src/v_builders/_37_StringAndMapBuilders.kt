db0d763aaf8535fecef0aed64b2abb583466e3de
James Lucas <jamesmlucas@gmail.com>
2017-07-16 15:41:49 -0500

    36-7

--- src/v_builders/_37_StringAndMapBuilders.kt
+++ src/v_builders/_37_StringAndMapBuilders.kt
@@ -28,12 +28,17 @@ fun todoTask37(): Nothing = TODO(
     """
 )
 
+fun buildMap(build: HashMap<Int, String>.() -> Unit): Map<Int, String> {
+    val map = HashMap<Int, String>()
+    map.build()
+    return map
+}
+
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        this.put(0, "0")
+        for (i in 1..10) {
+            this.put(i, "$i")
+        }
+    }
 }
